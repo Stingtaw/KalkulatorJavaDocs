9100a5c23944180b0d5458367d79235da76726a7
Stingtaw <kanta.10122315@mahasiswa.unikom.ac.id>
2025-11-04 01:12:25 +0700

    Hapus folder bin & docs dari tracking Git + tambahkan .gitignore

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.kalkulator","l":"Calculator"},{"p":"com.kalkulator","l":"MainApp"}];updateSearchResults();
